407e7e1a7fdc79cf7c045ef61a8944df07bc96cd
sanseongKo <tkstjd56@naver.com>
2024-03-10 23:36:01 +0900

    6일차 - test practice (3-2)

--- cafekiosk/src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
+++ cafekiosk/src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -2,6 +2,11 @@ package sample.cafekiosk.spring.api.service.mail;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 import sample.cafekiosk.spring.client.mail.MailSendClient;
 import sample.cafekiosk.spring.domain.history.mail.MailSendHistory;
 import sample.cafekiosk.spring.domain.history.mail.MailSendHistoryRepository;
@@ -11,21 +16,28 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class MailServiceTest {
+
+    @Spy
+    private MailSendClient mailSendClient;
+
+    @Mock
+    private MailSendHistoryRepository mailSendHistoryRepository;
+
+    @InjectMocks
+    private MailService mailService;
 
     @DisplayName("메일 전송 테스트")
     @Test
     void sendMail() {
         //given
-        MailSendClient mailSendClient = mock(MailSendClient.class);
         //Mock객체에 아무것도 지정하지 않으면, 기본값이 들어간다
-        MailSendHistoryRepository mailSendHistoryRepository = mock(MailSendHistoryRepository.class);
+//        when(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
+//                .thenReturn(true);
 
-        MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
-
-        when(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
-                .thenReturn(true);
-
+        doReturn(true).when(mailSendClient)
+                .sendEmail(anyString(), anyString(), anyString(), anyString());
         //when
         boolean result = mailService.sendMail("", "", "", "");
 
